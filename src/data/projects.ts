ed64069b56b0a791a0c016ce3f4f9184e92c30af
Luis Arzola <luisarzola3@gmail.com>
2024-09-30 17:25:37 -0700

    Typo

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -35,7 +35,7 @@ export const PROJECTS:Project[] =[
             },
             {
                 id:"tailwind",
-                name:"tailwind"
+                name:"Tailwind"
             },
         ]
     },
